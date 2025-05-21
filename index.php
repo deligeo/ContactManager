b96c4534df7e852997b2daf04cb58ff61d6cb72c
Delna George <Delnaelizabeth.George@triosstudent.com>
2025-05-21 11:10:55 -0400

    Completed index.php

--- index.php
+++ index.php
@@ -1,26 +1,48 @@
+<?php
+    session_start();
+    require("database.php");
+    $queryContacts = 'SELECT * FROM contacts';
+    $statement1 = $db->prepare($queryContacts);
+    $statement1->execute();
+    $contacts = $statement1->fetchAll();
+
+    $statement1->closeCursor();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Manager - Home</title>
-  <link rel="stylesheet" type="txt/css" href="css/main.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Manager - Home</title>
+    <link rel="stylesheet" type="txt/css" href="css/main.css"/>
 </head>
 <body>
-  <?php include("header.php"); ?>
-  <main>
-    <h2>Contact List</h2>
-    <table>
-      <tr>
-        <th>First name</th>
-        <th>Last Name</th>
-        <th>Email Address</th>
-        <th>Phone Number</th>
-        <th>Status</th>
-        <th>Birth Date</th>
-      </tr>
-    </table>
-  </main>
-  <?php include ("footer.php"); ?>
+    <?php include("header.php"); ?>
+    <main>
+        <h2>Contact List</h2>
+        <table>
+            <tr>
+                <th>First name</th>
+                <th>Last Name</th>
+                <th>Email Address</th>
+                <th>Phone Number</th>
+                <th>Status</th>
+                <th>Birth Date</th>
+            </tr>
+
+            <?php foreach ($contacts as $contact):?>
+                <tr>
+                    <td><?php echo $contact['firstName'];?></td>
+                    <td><?php echo $contact['lastName'];?></td>
+                    <td><?php echo $contact['emailAddress'];?></td>
+                    <td><?php echo $contact['phoneNumber'];?></td>
+                    <td><?php echo $contact['status'];?></td>
+                    <td><?php echo $contact['dob'];?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </main>
+    <?php include ("footer.php"); ?>
 </body>
 </html>
