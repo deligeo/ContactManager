56a6b9bdc8e670d4951f8c606bb7be9831ab96a5
Delna George <Delnaelizabeth.George@triosstudent.com>
2025-05-20 20:16:09 -0400

    Added header and footer

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Contact Manager - Home</title>
+  <link rel="stylesheet" type="txt/css" href="css/main.css"/>
 </head>
 <body>
-  
+  <?php include("header.php"); ?>
+  <main>
+    <h2>Contact List</h2>
+  </main>
+  <?php include ("footer.php"); ?>
 </body>
 </html>
